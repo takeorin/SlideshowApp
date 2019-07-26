7e93bdec89bc77c676bd9117404eeff537a9185f
takeorin <takemichi2626@gmail.com>
2019-07-26 21:58:22 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Takemichi on 2019/07/26.
+//  Copyright © 2019 takemichi.niro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
