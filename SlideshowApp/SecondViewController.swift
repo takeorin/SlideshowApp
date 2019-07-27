e18442e6be102550a3a4996af299709d364f94e2
takeorin <takemichi2626@gmail.com>
2019-07-27 12:53:06 +0900

    構築

---
+++ SlideshowApp/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  SlideshowApp
+//
+//  Created by Takemichi on 2019/07/27.
+//  Copyright © 2019 takemichi.niro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var expandedImage: UIImageView!
+    
+    var selectedimage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        expandedImage.image = selectedimage
+        
+    }
+    
+    
+    @IBAction func goBack(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var imageNo: Int = 0
+    var timer: Timer!
+    
+    @IBOutlet weak var slideView: UIImageView!
+    @IBOutlet weak var startstop: UIButton!
+    @IBOutlet weak var proceedbutton: UIButton!
+    @IBOutlet weak var backbutton: UIButton!
+    
+    //画像の配列
+    var imageArray:[UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for i in 1...3 {
+            imageArray.append(UIImage(named: "\(i).jpg")!)
+        }
+        //初期画像表示
+        slideView.image = imageArray[imageNo]
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "result" {
+            
+            let nextView = segue.destination as! SecondViewController
+            
+            nextView.selectedimage = slideView.image
+    
+        }
+    }
+    
+    @IBAction func tapImage(_ sender: UITapGestureRecognizer) {
+        
+        performSegue(withIdentifier: "result", sender: nil)
+    }
+    
+    @IBAction func proceed(_ sender: UIButton) {
+        if imageNo == 0 || imageNo == 1 {
+            imageNo += 1
+            slideView.image = imageArray[imageNo]
+        } else {
+            imageNo = 0
+            slideView.image = imageArray[imageNo]
+        }
+    }
+    
+    @IBAction func back(_ sender: UIButton) {
+        if imageNo == 1 || imageNo == 2 {
+            imageNo -= 1
+            slideView.image = imageArray[imageNo]
+        } else {
+            imageNo = 2
+            slideView.image = imageArray[imageNo]
+        }
+    }
+    
+    @IBAction func start(_ sender: UIButton) {
+        if timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            
+            startstop.setTitle("停止", for: .normal)
+            
+            proceedbutton.isEnabled = false
+            backbutton.isEnabled = false
+            
+        } else {
+            timer.invalidate()
+            timer = nil
+            startstop.setTitle("再生", for: .normal)
+            
+            proceedbutton.isEnabled = true
+            backbutton.isEnabled = true
+            
+        }
+    }
+    
+    @objc func updateTimer(_ timer: Timer) {
+        if imageNo == 0 || imageNo == 1 {
+            imageNo += 1
+            slideView.image = imageArray[imageNo]
+        } else {
+            imageNo = 0
+            slideView.image = imageArray[imageNo]
+        }
+    }
 }
-
